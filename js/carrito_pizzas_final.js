77f8bdeeb51d6d3f90ea0027650bd10e9dae1e31
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2022-11-09 20:48:40 -0300

    Creando funciones para darle funcionalidad al carrito de compras

--- js/carrito_pizzas_final.js
+++ js/carrito_pizzas_final.js
@@ -53,17 +53,106 @@ const detectarBotones = (data) => {
             const producto = data.find(item => item.id === parseInt(btn.dataset.id));
             producto.cantidad = 1;
             
-            if(carrito.hasOwnProperty(producto.id)){
-                producto.cantidad = carrito[producto.id].cantidad + 1;
-            }
-             
+            if(carrito.hasOwnProperty(producto.id)) producto.cantidad = carrito[producto.id].cantidad + 1;
+            
             carrito[producto.id] = {...producto}
-
-            pintarCarrito();
+            
+              pintarCarrito();
 
         });
     });
 }
 
+
 //El sexto paso es crear la función pintar carrito, encargada de pintar el carrito
 
+const $items = d.getElementById("items");
+
+const pintarCarrito = () => {
+
+    $items.innerHTML = "";
+
+    const $template = d.getElementById("template-carrito").content,
+        $fragment = d.createDocumentFragment();
+
+    //transformamos el carrito en un arreglo para recorrerlo con forEach
+    Object.values(carrito).forEach(producto => {
+        pintarItemCarrito($template, $fragment, producto);
+    });
+
+    $items.appendChild($fragment);
+
+    pintarPieDeCarrito();
+    botonesSumarRestar();
+
+}
+
+const pintarItemCarrito = (template, fragment, producto) => {
+    let temp = template
+    temp.querySelector("th").textContent = producto.id;
+    temp.querySelectorAll("td")[0].textContent = producto.nombre;
+    temp.querySelectorAll("td")[1].textContent = producto.cantidad;
+    temp.querySelector("span").textContent = producto.precio * producto.cantidad;
+
+    temp.querySelector(".btn-info").dataset.id = producto.id;
+    temp.querySelector(".btn-danger").dataset.id = producto.id;
+
+    const clone = temp.cloneNode(true);
+    fragment.appendChild(clone);
+
+}
+
+
+//El septimo paso es crear una función que pinte el pie del carrito donde estará el monto total y el botón de vaciar carrito
+
+const $pieDeCarrito = d.getElementById("footer-carrito");
+
+const pintarPieDeCarrito = () =>{
+
+    $pieDeCarrito.innerHTML = "";
+
+    if(Object.keys(carrito).length === 0){
+        $pieDeCarrito.innerHTML = 
+            `<th scope="row" colspan="5">Carrito vacío - Comience a comprar!</th>`;
+        
+        return;
+    }
+
+    const $template = d.getElementById("template-footer").content,
+        $fragment = d.createDocumentFragment();
+
+    $template.querySelectorAll("td")[0].textContent = sumarCantidades();
+    $template.querySelector("span").textContent = sumarPrecios();
+
+    const clone = $template.cloneNode(true);
+    $fragment.appendChild(clone);
+    $pieDeCarrito.appendChild($fragment);
+
+    vaciarCarrito();
+}
+
+//El octavo paso es crear una función que sume la cantidad total de productos agregados al carrito
+const sumarCantidades = () => {
+    const cantidadTotal = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0);
+    return cantidadTotal;
+}
+
+//El noveno paso es crear una función que sume la cantidad total de productos agregados al carrito
+const sumarPrecios = () => {
+    const precioTotal = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad*precio, 0);
+    return precioTotal;
+}
+
+//El decimo paso es crear una función que le de funcionalidad al boton de vaciar el carrito
+const vaciarCarrito = () => {
+    const $botonVaciarCarrito = d.getElementById("vaciar-carrito");
+    $botonVaciarCarrito.addEventListener("click", () => {
+        carrito = {};
+        pintarCarrito();
+    });
+}
+
+//El octavo paso es crear una función que le de funcionalidad a los botones de aumentar y disminuir cantidad en el carrito
+const botonesSumarRestar = () =>{
+    
+}
